ac6f7cb3d1bfb9b7dd451727de6cf88ede8adc7d
ericguo1988 <52126670+ericguo1988@users.noreply.github.com>
2019-06-29 13:09:23 +0800

    Update hellworld.py

--- hellworld.py
+++ hellworld.py
@@ -1,6 +1,13 @@
 def main():
     print("hello world!")
 
+def data_test():
+    list =[1,3,5,8,2,6,4]
+    for i in range(len(list)):
+        for j in range(len(list)-i-1):
+            if list[j]>list[j+1]:
+                list[j],list[j+1] = list[j+1],list[j]
+     return list
 
 if __name__=="__main__":
     main()
